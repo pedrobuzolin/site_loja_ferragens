05157deb29c2c7f0c38c7cfbaaaeadd4adc5cc36
pedrobuzolin <buzolinpedro@gmail.com>
2024-10-01 09:32:05 -0300

    Melhoria nas páginas de produtos

--- site/loja_ferragens/resources/views/site/secoes.blade.php
+++ site/loja_ferragens/resources/views/site/secoes.blade.php
@@ -2,27 +2,27 @@
 
 @section("conteudo")
             <!-- Fruits Shop Start-->
-            <div class="container-fluid fruite produtos py-5">
-            <div class="container py-5">
-                <div class="tab-class text-center">
-                    <div class="row g-4">
-                        <div class="col-lg-8 text-start">
-                            @if($secao)
-                                <h1>{{$secao->nomeSecao}}</h1>
-                            @endif
-                        </div>
+    <div class="container-fluid fruite produtos py-5">
+        <div class="container py-5">
+            <div class="tab-class text-center">
+                <div class="row g-4">
+                    <div class="col-lg-8 text-start">
+                        @if($secao)
+                            <h1>{{$secao->nomeSecao}}</h1>
+                        @endif
                     </div>
-                    <div class="tab-content">
-                        <div id="tab-1" class="tab-pane fade show p-0 active">
-                            <div class="row g-4">
-                                <div class="col-lg-12">
-                                    <div class="row g-4">
+                </div>
+                <div class="tab-content">
+                    <div id="tab-1" class="tab-pane fade show p-0 active">
+                        <div class="row g-4">
+                            <div class="col-lg-12">
+                                <div class="row g-4">
                                     @foreach ($produtos as $linha )
                                         <div class="col-md-6 col-lg-4 col-xl-3">
                                             <div class="rounded position-relative fruite-item">
                                                 <div class="fruite-img">
                                                     @if($linha->imagens)
-                                                        <img src="{{$linha->imagens->first()->linkImagem}}" class="img-fluid w-100 rounded-top" alt="">
+                                                        <img src="{{$linha->imagens->first()->linkImagem}}" class="img-fluid w-100 rounded-top" alt="{{$linha->nome}}">
                                                     @endif
                                                 </div>
                                                 <div class="text-white bg-secondary px-3 py-1 rounded position-absolute" style="top: 10px; left: 10px;">{{$secao->nomeSecao}}</div>
@@ -37,7 +37,6 @@
                                                 </div>
                                             </div>
                                         </div>
-                                    </div>
                                     @endforeach
                                 </div>
                             </div>
@@ -46,5 +45,6 @@
                 </div>      
             </div>
         </div>
+    </div>
         <!-- Fruits Shop End-->
 @stop
