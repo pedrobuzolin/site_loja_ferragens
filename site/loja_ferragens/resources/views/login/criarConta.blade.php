509d36bd7c39731bbbdb97e42f9448fc856949dc
pedrobuzolin <buzolinpedro@gmail.com>
2024-10-16 14:38:21 -0300

    Melhorias nas views de login e criar conta

--- site/loja_ferragens/resources/views/login/criarConta.blade.php
+++ site/loja_ferragens/resources/views/login/criarConta.blade.php
@@ -1,45 +1,45 @@
-@extends("layout_adm.topo_rodape")
-@section("conteudo")
-
-<body>
-  <!--  Body Wrapper -->
-  <div class="page-wrapper" id="main-wrapper" data-layout="vertical" data-navbarbg="skin6" data-sidebartype="full"
-    data-sidebar-position="fixed" data-header-position="fixed">
-    <div
-      class="position-relative overflow-hidden radial-gradient min-vh-100 d-flex align-items-center justify-content-center">
-      <div class="d-flex align-items-center justify-content-center w-100">
-        <div class="row justify-content-center w-100">
-          <div class="col-md-8 col-lg-6 col-xxl-3">
-            <div class="card mb-0">
-              <div class="card-body">
-                <a href="/" class="text-nowrap logo-img text-center d-block py-3 w-100">
-                  <img src="/layout_adm/images/logos/logo_preta.svg" alt="" style="width: 100px; height: 100px;">
-                </a>
-                <form method="post" >
-                    <div class="mb-3">
-                    <label for="nome" class="form-label">Nome</label>
-                    <input type="text" class="form-control" id="nome" aria-describedby="emailHelp">
-                  </div>
-                  <div class="mb-3">
-                    <label for="email" class="form-label">E-mail</label>
-                    <input type="email" class="form-control" id="email" aria-describedby="emailHelp">
-                  </div>
-                  <div class="mb-4">
-                    <label for="senha" class="form-label">Senha</label>
-                    <input type="senha" class="form-control" id="senha">
-                  </div>
-                  <button type="submit" class="btn btn-primary w-100 py-8 fs-4 mb-4">Criar Conta</button>
-                  <a href="login" class="btn btn-danger w-100 py-8 fs-4 mb-4">Voltar</a>
-                </form>
-              </div>
+@extends('layout_adm.topo_rodape')
+@section('conteudo')
+    <div class="page-wrapper" id="main-wrapper" data-layout="vertical" data-navbarbg="skin6" data-sidebartype="full"
+        data-sidebar-position="fixed" data-header-position="fixed">
+        <div
+            class="position-relative overflow-hidden radial-gradient min-vh-100 d-flex align-items-center justify-content-center">
+            <div class="d-flex align-items-center justify-content-center w-100">
+                <div class="row justify-content-center w-100">
+                    <div class="col-md-8 col-lg-6 col-xxl-3">
+                        <div class="card mb-0">
+                            <div class="card-body">
+                                <a href="/" class="text-nowrap logo-img text-center d-block py-3 w-100">
+                                    <img src="/layout_adm/images/logos/logo_preta.svg" alt=""
+                                        style="width: 100px; height: 100px;">
+                                </a>
+                                <h4>Cadastro de Cliente</h4>
+                                <form method="post">
+                                    @csrf
+                                    <div class="mb-3">
+                                        <label for="nome_cliente" class="form-label">Nome</label>
+                                        <input type="text" class="form-control" name="nome_cliente" id="nome_cliente"
+                                            aria-describedby="nome">
+                                    </div>
+                                    <div class="mb-3">
+                                        <label for="email" class="form-label">E-mail</label>
+                                        <input type="email" class="form-control" name="email" id="email"
+                                            aria-describedby="emailHelp">
+                                    </div>
+                                    <div class="mb-4">
+                                        <label for="password" class="form-label">Senha</label>
+                                        <input type="password" class="form-control" name="password" id="password">
+                                    </div>
+                                    <button type="submit" class="btn btn-primary w-100 py-8 fs-4 mb-4">Criar
+                                        Conta</button>
+                                    <a href="login" class="btn btn-danger w-100 py-8 fs-4 mb-4">Voltar</a>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
     </div>
-  </div>
-  <script src="/layout_adm/libs/jquery/dist/jquery.min.js"></script>
-  <script src="/layout_adm/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/iconify-icon@1.0.8/dist/iconify-icon.min.js"></script>
-
+    </body>
 @endsection
--- site/loja_ferragens/resources/views/login/login.blade.php
+++ site/loja_ferragens/resources/views/login/login.blade.php
@@ -1,43 +1,43 @@
-@extends("layout_adm.topo_rodape")
-@section("conteudo")
-<body>
-  <!--  Body Wrapper -->
-  <div class="page-wrapper" id="main-wrapper" data-layout="vertical" data-navbarbg="skin6" data-sidebartype="full"
-    data-sidebar-position="fixed" data-header-position="fixed">
-    <div
-      class="position-relative overflow-hidden radial-gradient min-vh-100 d-flex align-items-center justify-content-center">
-      <div class="d-flex align-items-center justify-content-center w-100">
-        <div class="row justify-content-center w-100">
-          <div class="col-md-8 col-lg-6 col-xxl-3">
-            <div class="card mb-0">
-              <div class="card-body">
-                <a href="/" class="text-nowrap logo-img text-center d-block py-3 w-100">
-                  <img src="/layout_adm/images/logos/logo_preta.svg" alt="" style="width: 100px; height: 100px;">
-                </a>
-                <form>
-                  <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Usuário</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                  </div>
-                  <div class="mb-4">
-                    <label for="exampleInputPassword1" class="form-label">Senha</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1">
-                  </div>
-                  <div class="d-flex align-items-center justify-content-between mb-4">
-                    <a class="text-primary fw-bold" href="./index.html">Esqueceu a Senha?</a>
-                  </div>
-                  <a href="./index.html" class="btn btn-primary w-100 py-8 fs-4 mb-4">Entrar</a>
-                  <div class="d-flex align-items-center justify-content-center">
-                    <p class="fs-4 mb-0 fw-bold">Novo em Casa Edlin?</p>
-                    <a class="text-primary fw-bold ms-2" href="criarConta">Criar Conta</a>
-                  </div>
-                </form>
-              </div>
+@extends('layout_adm.topo_rodape')
+@section('conteudo')
+    <div class="page-wrapper" id="main-wrapper" data-layout="vertical" data-navbarbg="skin6" data-sidebartype="full"
+        data-sidebar-position="fixed" data-header-position="fixed">
+        <div
+            class="position-relative overflow-hidden radial-gradient min-vh-100 d-flex align-items-center justify-content-center">
+            <div class="d-flex align-items-center justify-content-center w-100">
+                <div class="row justify-content-center w-100">
+                    <div class="col-md-8 col-lg-6 col-xxl-3">
+                        <div class="card mb-0">
+                            <div class="card-body">
+                                <a href="/" class="text-nowrap logo-img text-center d-block py-3 w-100">
+                                    <img src="/layout_adm/images/logos/logo_preta.svg" alt=""
+                                        style="width: 100px; height: 100px;">
+                                </a>
+                                <form>
+                                    @csrf
+                                    <div class="mb-3">
+                                        <label for="email" class="form-label">Usuário</label>
+                                        <input type="email" class="form-control" name="email" id="email"
+                                            aria-describedby="emailHelp">
+                                    </div>
+                                    <div class="mb-4">
+                                        <label for="password" class="form-label">Senha</label>
+                                        <input type="password" class="form-control" name="password" id="password">
+                                    </div>
+                                    <div class="d-flex align-items-center justify-content-between mb-4">
+                                        <a class="text-primary fw-bold" href="#">Esqueceu a Senha?</a>
+                                    </div>
+                                    <a href="#" class="btn btn-primary w-100 py-8 fs-4 mb-4">Entrar</a>
+                                    <div class="d-flex align-items-center justify-content-center">
+                                        <p class="fs-4 mb-0 fw-bold">Novo em Casa Edlin?</p>
+                                        <a class="text-primary fw-bold ms-2" href="/criarConta">Criar Conta</a>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
     </div>
-  </div>
-
 @endsection
